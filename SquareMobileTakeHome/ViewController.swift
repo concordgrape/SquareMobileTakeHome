0cb42f2d644a113aad78e5b07081fe2098ccc55a
Sky Roth <sky.j.roth@gmail.com>
2022-11-09 15:07:04 -0500

    Init Project

---
+++ SquareMobileTakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareMobileTakeHome
+//
+//  Created by Sky Roth on 2022-11-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
